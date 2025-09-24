8277c2b6567f7f9227e10a69f46ebd4f70e4d3f8
Nicolai Müller <nicolaimue@gmail.com>
2025-09-24 16:12:54 +0200

    Add documentation
    Adds some comments about the configuration of
    the tasks.

--- evaluation/task_config.py
+++ evaluation/task_config.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 class TaskConfig(BaseModel):
     dataset_id: str
-    name: str
-    field: str
+    name: str       # If task name is "general", the whole dataset will be used
+    field: str      # In this case, the field will be an empty string
     static_persona: str
