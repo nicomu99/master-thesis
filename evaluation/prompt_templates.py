bc7ef196b9f319ceabdac34b09ef35915b448214
Nicolai Müller <nicolaimue@gmail.com>
2025-10-01 10:17:41 +0200

    Fixed prompt template newlines

--- evaluation/prompt_templates.py
+++ evaluation/prompt_templates.py
@@ -1,38 +1,34 @@
-STATIC_SHORT_TEMPLATE = """
-For the field of {task_type}, create a persona string containing one sentence, written in second person perspective.
-The persona string should start with "{persona_string}".\n\n
-"""
+STATIC_SHORT_TEMPLATE = (
+    "For the field of {task_type}, create a persona string containing one sentence, written in second person "
+    "perspective. The persona string should start with \"{persona_string}\"."
+)
 
-STATIC_LONG_TEMPLATE = """
-For the field of {task_type}, create a persona string containing three sentences, written in second person perspective.
-The persona string should start with "{persona_string}".\n\n
-"""
+STATIC_LONG_TEMPLATE = (
+    "For the field of {task_type}, create a persona string containing three sentences, written in second person "
+    "perspective. The persona string should start with \"{persona_string}\"."
+)
 
-DYNAMIC_SHORT_TEMPLATE = """
-For the following question, create a persona string containing one sentence, written in second person perspective.
-The persona string should start with "{persona_string}".\n\n
+DYNAMIC_SHORT_TEMPLATE = (
+    "For the field of {task_type}, create a persona string containing one sentence, written in second person "
+    "perspective. The persona string should start with \"{persona_string}\". The string should be tailored to "
+    "the question:\n \"{question}\""
+)
 
-{question}
-"""
+DYNAMIC_LONG_TEMPLATE = (
+    "For the following question, create a persona string containing three sentences, written in second person "
+    "perspective. The persona string should start with \"{persona_string}\".\n {question}"
+)
 
-DYNAMIC_LONG_TEMPLATE = """
-For the following question, create a persona string containing three sentences, written in second person perspective.
-The persona string should start with "{persona_string}".\n\n
+OPEN_QUESTION_TEMPLATE = "{question}"
 
-{question}
-"""
+MC_QUESTION_TEMPLATE = (
+    "{question}\n\n"
+    "(A) {choice_1}\n"
+    "(B) {choice_2}\n"
+    "(C) {choice_3}\n"
+    "(D) {choice_4}\n"
+)
 
-QUESTION_TEMPLATE = """
-{persona} Answer the following question:
-
-{question}
-"""
-
-MC_QUESTION_TEMPLATE = """
-{question}\n\n
-
-(A) {choice_1}\n
-(B) {choice_2}\n
-(C) {choice_3}\n
-(D) {choice_4}\n
-"""
+SUMMARIZATION_TEMPLATE = (
+    "Please summarize the following text: \n{text}"
+)
