27449f80ee19dccf3c1b1aa8a36e334b433bffe5
Nicolai Müller <nicolaimue@gmail.com>
2025-10-08 12:29:53 +0200

    Add docstrings

--- evaluation/dataset_config.py
+++ evaluation/dataset_config.py
@@ -5,15 +5,34 @@ from dataclasses import dataclass
 
 @dataclass
 class DatasetConfig:
+    """Dataset specific configuration class.
+    
+    Attributes:
+        dataset_id (str): String identifier of the dataset.
+        huggingface_id (str): Huggingface hub identifier of the dataset.
+        split (str): Split parameter controlling which portion should be loaded from huggingface.
+        question_type (str): Type of the question. Can be "open_question", "mc_question" and "summarization".
+        question_column (str): Column in the dataset containing the question of the sample.
+        answer_column (str | None): If the dataset contains multiple-choice samples, this attribute specifies which
+            column of the dataset contains the possible choices.
+        task_column (str | None): If a dataset contains several tasks, this attribute denotes which column contains the
+            identifier of each task. The name field in the TaskConfig class is used to identify rows that belong 
+            together.
+        load_name (str | None): Parameter load_name of the load_dataset function from the HF datasets library.
+    """
     dataset_id: str
     huggingface_id: str
     split: str
     question_type: str
-    question_field: str
-    answer_field: str
+    question_column: str
+    answer_column: Optional[str]
     task_column: Optional[str]
-    load_name: Optional[str] = None
+    load_name: Optional[str]
 
     def __post_init__(self):
         if self.task_column == "":
             self.task_column = None
+        if self.answer_column == "":
+            self.answer_column = None
+        if self.load_name == "":
+            self.load_name = None
--- evaluation/task_config.py
+++ evaluation/task_config.py
@@ -5,9 +5,21 @@ from dataclasses import dataclass
 
 @dataclass
 class TaskConfig:
+    """Data class containing task-specific configuration parameters.
+    
+    Attributes:
+        task_id (str): Identifier of the task of the format "<dataset_id>_<name>". 
+            If name is empty, "_all" is appended to the identifier instead.
+        dataset_id (str): Identifier of the dataset where samples of the task are stored.
+        name (None | str): Category identifier of the task. If the dataset of this task contains
+            several areas of expertise, this value is used to identify rows of the same expertise.
+            Else, name will be none.
+        field (str): High-level field of study of this task.
+        static_persona (str): A single-sentence, base persona string in the format "You are <occupation>". 
+    """
     task_id: str
     dataset_id: str
-    name: Optional[str]       # If task name is "general", the whole dataset will be used
+    name: Optional[str]
     field: str
     static_persona: str
 
