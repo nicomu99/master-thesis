74d7b66756285a0ef1175dbe9157da17d48ea43e
Nicolai Müller <nicolaimue@gmail.com>
2026-04-15 14:37:43 +0200

    Fix bug

--- inference/dataset_handler.py
+++ inference/dataset_handler.py
@@ -270,7 +270,7 @@ class DatasetHandler:
         if isinstance(subset_df, list):
             subset_df = pd.DataFrame(subset_df)
 
-        dataframe = dataframe.set_index(STATIC_ID_COLUMN)
+        dataframe.set_index(STATIC_ID_COLUMN, inplace=True)
         subset_df = subset_df.set_index(STATIC_ID_COLUMN)
         for col in subset_df.columns:
             if col not in dataframe.columns:
